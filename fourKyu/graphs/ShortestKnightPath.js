10423633f37efc8ad4acff0c4bb3ed83db3d9ea4
RASantos92 <rasbms81608@gmail.com>
2023-10-05 19:45:24 -0500

    update

--- fourKyu/graphs/ShortestKnightPath.js
+++ fourKyu/graphs/ShortestKnightPath.js
@@ -20,15 +20,16 @@ const buildProperChessBoard = () => {
 buildProperChessBoard()
 const knight = (start, finish) => {
     const chessBoard = buildProperChessBoard();
+    const listOfPickedMoves = [];
     const alphaKeys = {"a":0, "b":1, "c":2, "d":3, "e":4, "f":5, "g":6, "h":7};
     let initalPositionValue = chessBoard[start[1]-1][alphaKeys[start[0]]];
     const finalPostitionValue = chessBoard[finish[1]-1][alphaKeys[finish[0]]];
-    let xInitalPosition = start[1]-1
-    let yInitalPosition = alphaKeys[start[0]]
+    let yInitalPosition = start[1]-1
+    let xInitalPosition = alphaKeys[start[0]]
 
-    let xFinalPostion = finish[1]-1
-    let yFinalPostion = alphaKeys[finish[0]]
-    
+    let yFinalPostion = finish[1]-1
+    let xFinalPostion = alphaKeys[finish[0]]
+
     const listOfMovesKight = (chessBoard, x, y) => {
         // position values
         const output = [];
@@ -36,22 +37,48 @@ const knight = (start, finish) => {
         for(let i=0; i < moving.length; i++){
             if(i<2){
                 x += moving[i];
-                output.push(chessBoard[x][y+1]);
-                output.push(chessBoard[x][y-1]);
+                if(chessBoard[x] != undefined){
+                    output.push(chessBoard[x][y+1]);
+                    output.push(chessBoard[x][y-1]);
+                }
                 x -= moving[i]
             } else {
                 y += moving[i];
-                output.push(chessBoard[x+1][y]);
-                output.push(chessBoard[x-1][y]);
+                    chessBoard[x+1] !== undefined ? output.push(chessBoard[x+1][y]) : null;
+                    chessBoard[x-1] !== undefined ? output.push(chessBoard[x-1][y]): null;
                 y -= moving[i]
             }
         }
-        return output;
+        return output.filter((e) => e !== undefined);
     }
-    const listOfMoves = listOfMovesKight(chessBoard, xInitalPosition, yInitalPosition);
-    console.log(listOfMoves);
+    while(initalPositionValue != finalPostitionValue){
+        const listOfMoves = listOfMovesKight(chessBoard, yInitalPosition, xInitalPosition);
+        console.log(listOfMoves);
+        let smallestDifference = Infinity;
+        let smallestMove = "";
+        for(let i=0; i<listOfMoves.length; i++){
+            if(listOfMoves[i] === finalPostitionValue){
+                listOfPickedMoves.push(listOfMoves[i]);
+                return listOfPickedMoves.length;
+            }
+            const yValueOfMove = listOfMoves[i][1] -1;
+            const xValueOfMove = alphaKeys[listOfMoves[i][0]];
+            let yDifference = yValueOfMove - yFinalPostion;
+            let xDifference = xValueOfMove - xFinalPostion;
+            yDifference = yDifference < 0 ? yDifference * -1 : yDifference;
+            xDifference = xDifference < 0 ? xDifference * -1 : xDifference;
+            console.log("the difference", yDifference, xDifference)
+            const totalDifference = yDifference + xDifference;
+            if(totalDifference < smallestDifference){
+                smallestDifference = totalDifference;
+                smallestMove = listOfMoves[i];
+            }
+        }
+        console.log(smallestMove)
+        break
+    }
     
 
 }
 
-knight("a5", "b5")
+console.log(knight("a1", "b5"))
