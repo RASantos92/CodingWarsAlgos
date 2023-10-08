bcdac009bfadec33f7d54fb19bd69abae1c4b5ba
RASantos92 <rasbms81608@gmail.com>
2023-10-08 18:43:47 -0500

    finised mix

--- fourKyu/stringManipulation/stringsMix.js
+++ fourKyu/stringManipulation/stringsMix.js
@@ -1,16 +1,18 @@
+
 const mix = (s1, s2) => {
     let output = "";
+    const regex = /[A-Z + \ ]/g;
+    const s1NoCapitals = s1.replaceAll(regex, "");
+    const s2NoCapitals = s2.replaceAll(regex, "");
     const buildFrequencyTableOfTwoStrings = (s1, s2) => {
         const frequencyTable = {};
         for(let i = 0; i< (s1.length > s2.length ? s1.length : s2.length); i++){
-            if(s1[i] !== s1[i].toUpperCase()){
                 if(!frequencyTable[s1[i]]) {
                     frequencyTable[s1[i]] = s1[i];
                 }else{
                     frequencyTable[s1[i]] = frequencyTable[s1[i]] + s1[i]
                 }
-            }
-            if(s2[i] && s2[i] !== s2[i].toUpperCase()){
+            if(s2[i]){
                 if(!frequencyTable[s2[i]+"`"]) {
                     frequencyTable[s2[i]+"`"] = s2[i];
                 }else{
@@ -20,9 +22,9 @@ const mix = (s1, s2) => {
         }
         return frequencyTable;
     }
-    const frequencyTable = buildFrequencyTableOfTwoStrings(s1,s2);
+    const frequencyTable = buildFrequencyTableOfTwoStrings(s1NoCapitals,s2NoCapitals);
     for(const [key, value] of Object.entries(frequencyTable)){
-        if(value.length <= 1 || key[1] === "`") continue;
+        if((value.length <= 1 && frequencyTable[key+"`"]?.length <=1) || key[1] === "`") continue;
         let firstOutputValue = "";
         let secondOutputValue = "";
         if(value.length > frequencyTable[key+"`"]?.length){
@@ -33,8 +35,8 @@ const mix = (s1, s2) => {
             secondOutputValue = value;
         } else { firstOutputValue = "2"; secondOutputValue = frequencyTable[key+"`"] };
         console.log("hello", frequencyTable[key+"`"]?.length)
-
-        output +=  firstOutputValue + ":" + secondOutputValue + "/"
+        
+        secondOutputValue ? output +=  firstOutputValue + ":" + secondOutputValue + "/" : null;
     }
     console.log("output here",output)
     console.log(frequencyTable)
@@ -43,3 +45,8 @@ const mix = (s1, s2) => {
 mix("Are the kids at home? aaaaa fffff", "Yes they are here! aaaaa fffff")
 
 
+// const re = new RegExp("[a-z]")
+// const test = "TTTTTest"
+// const regex = /[A-Z]/g;
+// test.replaceAll(regex, "")
+// console.log(test.replaceAll(regex, ""))
