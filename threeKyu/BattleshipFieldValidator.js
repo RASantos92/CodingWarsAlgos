a54610f3b26c8486495fef255965bb6d69205fa5
RASantos92 <rasbms81608@gmail.com>
2023-10-27 01:19:52 -0500

    getting there

--- threeKyu/BattleshipFieldValidator.js
+++ threeKyu/BattleshipFieldValidator.js
@@ -33,24 +33,66 @@ const fieldValidator = (field) => {
             "found":0
         }
     }
+    // copy of the field to change the ships position to 0 after visiting the location
     const fieldCopy = [...field];
+    // double for loop to gain access to cordinace 
     for(let i = 0; i < field.length; i++){
         for(let j = 0; j < field[i].length; j++){
             const row = fieldCopy[i]
             //finding the start of the ship,
             // problem not revisiting the locations again
             if(row[j] === 1){
-                let count = 1;
+                let shipSize = 1;
                 const checkDiagonalPositionsValidation = (i, j) => {
                     if(fieldCopy[i+1][j-1] === 1 || fieldCopy[i+1][j+1] === 1) return false;
                     return true;
                 }
                 // finding direction
-                if(fieldCopy[i][j+1] === 1){
-                    
+                const leftDirection = fieldCopy[i][j+1];
+                const verticalDirection = fieldCopy[i+1][j];
+
+
+                // leftDirection - this is ment to get the shipSize and to validate the ships position
+                const checkLowerPositionsForLeftDirection = (i, j) => {
+                    if(fieldCopy[i+1][j] == 1 || fieldCopy[i+1][j+1] == 1){
+                        return false;
+                    }
+                    return true;
                 }
-                let isPostitionValid = checkDiagonalPositionsValidation(i,j);
-                var isHorazontal = 
+                if(leftDirection === 1){
+                    let nextCheckValue = j+1;
+                    while(row[nextCheckValue] == 1){
+                        const isPositionValid = checkLowerPositionsForLeftDirection(i,nextCheckValue);
+                        if(isPositionValid){
+                            shipSize++;
+                            // this will patch the 1 value to a zero value in the fieldCopy
+                            row[nextCheckValue] = 0;
+                            nextCheckValue++
+                        }
+                    }
+                }
+
+                // verticalDirection - This is ment to get the shipSize and to validate the ships position if found to be vertical.
+                const checkHorazontalPositionsForVerticalDirection = (i,j) => {
+                    if(fieldCopy[i][j+1] == 1 || fieldCopy[i][j-1] == 1){
+                        return false;
+                    }
+                    return true;
+                }
+                if(verticalDirection == 1 && shipSize == 1){
+                    let nextCheckValue = i+1;
+                    while(fieldCopy[nextCheckValue][j] == 1){
+                        const isPositionValid = checkHorazontalPositionsForVerticalDirection(nextCheckValue,j);
+                        if(isPositionValid){
+                            shipSize++;
+                            fieldCopy[nextCheckValue][j]=0;
+                            nextCheckValue++;
+                        }
+                    }
+                }
+
+                // let isPostitionValid = checkDiagonalPositionsValidation(i,j);
+                // var isHorazontal = 
             }
         }
     }
